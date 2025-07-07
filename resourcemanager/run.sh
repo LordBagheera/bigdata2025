78e86120b5cac4a938c3e5219466c9c8431513e1
LordBagheera <erik.hurtado@inacapmail.cl>
2025-07-07 17:42:24 -0400

    Hive ready

--- resourcemanager/run.sh
+++ resourcemanager/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export YARN_RESOURCEMANAGER_OPTS="$YARN_NODEMANAGER_OPTS --add-opens java.base/java.lang=ALL-UNNAMED"
+export YARN_RESOURCEMANAGER_OPTS="$YARN_NODEMANAGER_OPTS --add-opens java.base/java.net=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED --add-opens java.base/java.util.concurrent.atomic=ALL-UNNAMED"
 $HADOOP_HOME/bin/yarn --config $HADOOP_CONF_DIR resourcemanager
